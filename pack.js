920c0fc14ef34e6fa386f89e1603adb81e251221
Sean <SeanAnthonyMiller@gmail.com>
2016-06-03 20:08:58 +1000

    Release 3.7.7

--- pack.js
+++ pack.js
@@ -1,12 +1,12 @@
-var fs = require('fs.extra');
+var fs = require('fs-extra');
 
-fs.rmrf('./when', function (err) {
+fs.emptyDirSync('./when', function (err) {
   if (err) {
     throw err;
   }
 });
 
-fs.copyRecursive('./node_modules/when/dist/browser', './when', function (err) {
+fs.copySync('./node_modules/when/dist/browser', './when', {clobber: true}, function (err) {
   if (err) {
     throw err;
   }
